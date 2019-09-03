36ce7942d09c013b51007a057dc06f0b0b4eaecf
Roman Gruba <roman.gruba@gmail.com>
2019-09-03 20:39:13 +0300

    Updates

---
+++ precache-manifest.83961d3b3df8e7e3cd8147d26d716cfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ba660e7608bdfc545118d23cff0512",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "4e50a057c765ae6bdc73",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "4e50a057c765ae6bdc73",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.57d43d8d.chunk.js"
+  },
+  {
+    "revision": "a09a4ae40106a803737c",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.3ae77821.chunk.js"
+  },
+  {
+    "revision": "ff88846bb3f73bf7dfff",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime~main.fa426ac3.js"
+  }
+]);
